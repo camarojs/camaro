c1ecedd292585ba58c228cc40ca60449890f7e17
Dunn <wg0121@outlook.com>
2024-10-21 11:55:06 +0000

    test: update test run options

--- test/index.ts
+++ test/index.ts
@@ -6,7 +6,17 @@ import { spec } from "node:test/reporters";
 
 const stream = run({
     concurrency: true,
+    // @ts-expect-error remove when types are updated
+    execArgv: ["--enable-source-maps"],
     globPatterns: ["test/**/*.test.ts"],
+
+    coverage: true,
+    coverageExcludeGlobs: ["test/**/*.test.ts"],
+
+    branchCoverage: 90,
+    functionsCoverage: 90,
+    lineCoverage: 90,
+
     setup(reporter) {
         reporter.on("test:fail", () => {
             process.exitCode = 1;
