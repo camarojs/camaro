27b1248bd2b0d44f471d0339bac9f3552705e89f
Dunn <wg0121@outlook.com>
2025-11-22 22:47:47 +0800

    feat: implement TrieNode and TrieRouter classes with basic routing functionality

---
+++ packages/core/src/router/trie-node.ts
@@ -0,0 +1,16 @@
+export default class TrieNode {
+    /**
+     * Children nodes mapped by path segment
+     */
+    children = new Map<string, TrieNode>();
+    /**
+     * Handlers associated with this node, mapped by HTTP method
+     */
+    handlers = new Map<string, CallableFunction>();
+
+    /**
+     * Creates a new TrieNode
+     * @param paramName Parameter name if this node represents a dynamic segment
+     */
+    constructor(public paramName?: string) { }
+}
---
+++ packages/core/src/router/trie-router.ts
@@ -0,0 +1,62 @@
+import { METHODS } from "node:http";
+import TrieNode from "./trie-node.js";
+
+export default class TrieRouter {
+    #root = new TrieNode();
+
+    add(path: string, method: string, handler: CallableFunction) {
+        if (!METHODS.includes(method.toUpperCase())) {
+            throw new Error(`Invalid HTTP method: ${method}`);
+        }
+
+        const segments = path.split("/").filter(Boolean);
+        let node = this.#root;
+
+        for (const segment of segments) {
+            let key = segment;
+            let paramName;
+
+            if (segment.startsWith(":")) {
+                key = "*";
+                paramName = segment.slice(1);
+            }
+
+            if (!node.children.has(key)) {
+                const node = new TrieNode(paramName);
+                node.children.set(key, node);
+            }
+
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            node = node.children.get(key)!;
+        }
+
+        node.handlers.set(method.toUpperCase(), handler);
+    }
+
+    find(path: string, method: string) {
+        const segments = path.split("/").filter(Boolean);
+        const params: Record<string, string> = {};
+        let node = this.#root;
+
+        for (const segment of segments) {
+            if (node.children.has(segment)) {
+                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                node = node.children.get(segment)!;
+            }
+            else if (node.children.has("*")) {
+                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                node = node.children.get("*")!;
+                if (node.paramName) {
+                    params[node.paramName] = segment;
+                }
+            }
+            else {
+                return null;
+            }
+        }
+
+        const handler = node.handlers.get(method.toUpperCase());
+
+        return handler ? { handler, params } : null;
+    }
+}
