aa18575da5b27121b5d4cbfae941863906960aa6
Dunn <wg0121@outlook.com>
2025-12-12 20:17:53 +0800

    feat(eslint)!: restructure exports (#10)
    * feat(eslint-config): remove custom restrict-template-expressions rule
    
    * feat(eslint-config): restructure exports and add common TypeScript ESLint configuration

---
+++ packages/eslint-config/src/typescript.ts
@@ -0,0 +1,24 @@
+import type { TSESLint } from "@typescript-eslint/utils";
+import eslintTS from "typescript-eslint";
+import { common } from "./common.js";
+
+const eslintTsRules = [
+    ...eslintTS.configs.recommendedTypeChecked,
+    ...eslintTS.configs.strictTypeChecked,
+    ...eslintTS.configs.stylisticTypeChecked,
+].reduce<TSESLint.FlatConfig.Rules>((acc, config) => ({ ...acc, ...config.rules }), {});
+
+export const ts: TSESLint.FlatConfig.ConfigArray = [
+    ...common,
+    {
+        files: ["**/*.ts"],
+        languageOptions: {
+            parser: eslintTS.parser,
+            parserOptions: { projectService: true },
+        },
+        plugins: { "@typescript-eslint": eslintTS.plugin },
+        rules: {
+            ...eslintTsRules,
+        },
+    },
+];
