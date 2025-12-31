9428fbbbe96ffd591a39be23e004c6f11b030b52
Dunn <wg0121@outlook.com>
2025-12-31 14:07:48 +0800

    feat(core): add compose utility for middleware function composition

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -1,5 +1,16 @@
 import { Class } from "type-fest";
 
+export type ComposedMiddleware = (context: Context, next?: Next) => Promise<void>;
+
+// TODO: refine Context type
+export type Context = Record<string, unknown>;
+
+export type Handler = (ctx: Context) => Promise<void> | void;
+
+export type Middleware = (ctx: Context, next: Next) => Promise<void> | void;
+
+export type Next = () => Promise<void> | void;
+
 export enum ServiceLifetime {
     Transient = 0,
     Scoped = 1,
