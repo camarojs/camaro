06bbc876801a2d49aa30cd82992ec83e16c7f225
Dunn <wg0121@outlook.com>
2026-02-09 10:53:25 +0800

    feat(eslint-config): update common and typescript lint configurations to use defineConfig

--- packages/eslint-config/src/typescript.ts
+++ packages/eslint-config/src/typescript.ts
@@ -1,31 +1,29 @@
 import type { TSESLint } from "@typescript-eslint/utils";
 import eslintTS from "typescript-eslint";
 import { common } from "./common.js";
-
-const eslintTsRules = [
-    ...eslintTS.configs.recommendedTypeChecked,
-    ...eslintTS.configs.strictTypeChecked,
-    ...eslintTS.configs.stylisticTypeChecked,
-].reduce<TSESLint.FlatConfig.Rules>((acc, config) => ({ ...acc, ...config.rules }), {});
+import { defineConfig } from "eslint/config";
 
 export interface TypescriptLintConfigOptions {
     files: string[];
 }
 
 export const createTypescriptLintConfig = (options: TypescriptLintConfigOptions): TSESLint.FlatConfig.ConfigArray => {
-    return [
-        ...common,
+    return defineConfig(
+        common,
         {
             files: options.files,
+            extends: [
+                eslintTS.configs.recommendedTypeChecked,
+                eslintTS.configs.strictTypeChecked,
+                eslintTS.configs.stylisticTypeChecked,
+            ],
             languageOptions: {
                 parser: eslintTS.parser,
                 parserOptions: { projectService: true },
             },
-            plugins: { "@typescript-eslint": eslintTS.plugin },
             rules: {
-                ...eslintTsRules,
                 "@typescript-eslint/consistent-type-imports": ["error", { fixStyle: "inline-type-imports" }],
             },
         },
-    ];
+    );
 };
